845fc918f35ab5d17f8378b4a9c9a75b45fc0cd1
Eleodor <dariusdoroftei408@gmail.com>
2026-08-09 01:13:19 +0300

    modificari minore

--- Verificator de parole/Verificator de parole.py
+++ Verificator de parole/Verificator de parole.py
@@ -6,28 +6,28 @@ print("Parola introdusa este: " + parola)
 print("Lungime: " + str(len(parola))+" caractere")
 print("Cel putin 8 caractere: " + str(len(parola) >= 8))
 litere_mari=False
-for conditie1 in range(len(parola)):
-    if(not parola[conditie1].isalpha()):
+for i in range(len(parola)):
+    if(not parola[i].isalpha()):
         continue
-    if(parola[conditie1]==parola[conditie1].upper()):
+    if(parola[i]==parola[i].upper()):
         litere_mari=True
 print("Are litere mari: "+str(litere_mari))    
 litere_mici=False
-for conditie1 in range(len(parola)):
-    if(not parola[conditie1].isalpha()):
+for i in range(len(parola)):
+    if(not parola[i].isalpha()):
         continue
-    if(parola[conditie1]==parola[conditie1].lower()):
+    if(parola[i]==parola[i].lower()):
         litere_mici=True
 print("Are litere mici: "+str(litere_mici)) 
 doar_litere=True
-for conditie1 in range(len(parola)):
-    if(parola[conditie1].isalpha()==False or parola[conditie1]==" "):
+for i in range(len(parola)):
+    if(parola[i].isalpha()==False or parola[i]==" "):
         doar_litere=False
         break
 print("Are doar litere(vrem False): "+ str(doar_litere))   
 doar_spatii=False
-for conditie1 in range(len(parola)):
-    if(parola[conditie1]==" "):
+for i in range(len(parola)):
+    if(parola[i]==" "):
         doar_spatii=True
         break
 print("Are spatii(vrem False): "+str(doar_spatii))
